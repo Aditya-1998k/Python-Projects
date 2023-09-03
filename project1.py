df827ffcc29313b0c62a1a1a20471a3a5a0cdbc2
aditya <aditya98gupta@gmail.com>
2023-09-03 21:30:27 +0530

    Youtube Video Downloader

---
+++ project1.py
@@ -0,0 +1,39 @@
+""""
+Youtube video and Audio Downloader
+"""
+import tkinter as tk
+from tkinter import messagebox
+from pytube import YouTube
+
+def download_video():
+    url = entry_url.get()
+    output_path= entry_output_path.get()
+    try:
+        yt = YouTube(url)
+        video_stream = yt.streams.get_highest_resolution()
+        video_stream.download(output_path)
+        messagebox.showinfo("Downloaded", "Downloaded Successfully")
+    except Exception as e:
+        messagebox.showerror("Error", f"Some Error occured: {e}")
+
+
+root = tk.Tk()
+root.title("Youtube Video Downloader")
+
+label_url = tk.Label(root, text="Enter the YouTube Video URL: ")
+label_url.pack()
+
+entry_url = tk.Entry(root, width=50)
+entry_url.pack()
+
+label_output_path= tk.Label(root, text="Enter the Output Path: ")
+label_output_path.pack()
+
+
+entry_output_path = tk.Entry(root, width=50)
+entry_output_path.pack()
+
+download_button = tk.Button(root, text="Download", command=download_video)
+download_button.pack()
+
+root.mainloop()
